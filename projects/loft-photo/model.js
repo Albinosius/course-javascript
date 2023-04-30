4cc97e09bbd62f7083bbac9d2cb005cf53396c5d
Albina Akhmetova <albinosiusp@gmail.com>
2023-04-30 13:53:05 +0500

    h/w week 2 (loft-photo)

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,21 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!array.lenght) {
+      return null;
+    }
+    
+    const ix = parseInt(Math.random() * (array.lenght - 1));
+
+    return array[ix];
+  },
+
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB[friend.id];
+    const photo = this.getRandomElement(photos);
+
+    return {friend, url: photo.url};
+  },
 };
